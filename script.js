cb5f6d561ba1b4643fbfe0efda12322b3d7b7b47
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-12-05 11:02:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let arr1 = [3, 7, 12, 20];
+let arr2 = [1, 5, 8, 18, 25];
+let ans = [];
+while(arr1.length > 0 || arr2.length > 0){
+	while(arr1.length > 0 && arr2.length > 0){
+		if(arr1[0] < arr2[0]){
+			ans.push(arr1[0].shift());
+		}
+		else{
+			ans.push(arr2[0].shift());
+		}
+	}
+
+	if( arr1.length > 0 ){
+		while(arr1.length > 0){
+			ans.push(arr1[0].shift());
+		}
+	}
+}
